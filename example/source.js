d13139bc2971a45420e855a4cb548f028e0d6e81
Jean Claudette Ambait <jeanclaudetteambait@gmail.com>
2017-06-15 22:26:02 +1200

    WIP example implementation app

---
+++ example/source.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Button } from 'tiny-button';
+
+ReactDOM.render( <Button>This is a button.</Button>, document.getElementById( 'react-app' ) );
